f647803be4b37fd1eb591d756b4e8256ce847e15
Hannah Malm <hannahemalm616@gmail.com>
2021-10-11 22:05:58 -0400

    commit tree b1ff73798a0b434acf9b6c0e3864cd01c8f0cf5a

--- app/controllers/neighborhoods_controller.rb
+++ app/controllers/neighborhoods_controller.rb
@@ -1,2 +1,18 @@
 class NeighborhoodsController < ApplicationController
+
+    #instance method - knows all the avialable listings given a date range
+
+    self.highest_ratio_res_to_listings
+    #knows the neighborhood with the higest ratio of reservations to listings
+    #doesn't hardcode the neighborhood with the highest ratio
+
+    end 
+
+    self.most_rest 
+    #knows the neighborhood with the most reservations 
+    #doesn't hardcode the neighborhood with the most reservations
+    @reservation.higest
+
+    end 
+
 end
--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -3,4 +3,20 @@ class Reservation < ActiveRecord::Base
   belongs_to :guest, :class_name => "User"
   has_one :review
 
+  validates :checkin, presence: true
+  validates :checkout, presence: true
+
+  #validates that you cannot make a reservation on your own listing
+
+  #validates that a listing is available at checkin before making reservation 
+  before_save :reservation_creation
+
+  #validates that listing is available at checkout before making reservation
+  before_save :reservation_creation
+
+
+  private 
+    def reservation_creation 
+
+    end 
 end
